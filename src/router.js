23d970b12a8e474ed8401a5a3662bba771c15d2f
Sergey Eroshenkov <techmeat@ya.ru>
2019-03-23 15:51:57 +0300

    Change routing

--- src/router.js
+++ src/router.js
@@ -1,22 +1,36 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Swimming from './views/Swimming.vue';
 
 Vue.use(Router);
 
+const SPORTS = ['swimming'];
+const BOARDS = SPORTS.map(sport => {
+  return {
+    path: '/' + sport,
+    component: () => import('./views/Match'),
+    children: [
+      { path: '',
+        name: sport,
+        component: () => import('./modules/' + sport),
+      },
+    ]
+  };
+});
+
 export default new Router({
   mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: () => import('./views/Home'),
     },
+    ...BOARDS,
     {
-      path: '/swimming',
-      name: 'swimming',
-      component: Swimming,
-    },
+      path: '*',
+      name: '404',
+      component: () => import('./views/PageNotFound'),
+    }
   ],
 });
